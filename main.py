b2455b263cb47623a76210d610a41f9c01f660a6
RoboPhot_win <nikita.googlov@gmail.com>
2023-11-06 11:46:08 +0500

    w

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from rppl_data import RPPLData
 
 my_data = RPPLData()
 my_data.list_fits_files()
-my_data.make_master_dark()
-my_data.make_master_flat()
+# my_data.make_master_dark()
+# my_data.make_master_flat()
 my_data.write_images_table("ImagesTable.txt")
 my_data.apply_calibration()
 
